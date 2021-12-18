8628fda6a7c2dce38de9eb20d1c23998bfa62ba4
Md Shah Jalal <bestwebdesignerr@gmail.com>
2021-12-18 23:58:30 +0600

    about dev

--- About.js
+++ About.js
@@ -3,8 +3,13 @@ import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 
 const About = ({ navigation }) => {
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>About page</Text>
+        <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+            <View style={{ width: 130, height: 130, backgroundColor: 'green', justifyContent: 'center', alignItems: 'center', borderRadius: 20, marginRight: 10 }}>
+                <Text style={{ color: 'white', paddingHorizontal: 5, fontSize: 20, fontWeight: 'bold', textAlign: 'center' }}>ABOUT CAR APP</Text>
+            </View>
+            <View style={{ width: 130, height: 130, backgroundColor: 'green', justifyContent: 'center', alignItems: 'center', borderRadius: 20 }}>
+                <Text style={{ color: 'white', paddingHorizontal: 5, fontSize: 20, fontWeight: 'bold', textAlign: 'center' }}>ABOUT DEVELOPER</Text>
+            </View>
         </View>
     )
 }
